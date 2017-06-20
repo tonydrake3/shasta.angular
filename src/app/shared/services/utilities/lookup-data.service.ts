5bee6cae1c3c1188baae6f32d2d4dfeeb45dd787
Alex T <alext@esub.com>
2017-06-20 09:14:53 -0700

    [SH-835] Generic service for lookup data

---
+++ src/app/shared/services/utilities/lookup-data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LookupDataService {
+
+    constructor () {}
+
+    getTimesheetMenu () {
+
+    }
+
+    getProjectsMenu () {
+        return Promise((resolve,reject) => {
+            
+        })
+    }
+}
