3ab92435166fb4b4a154f2074966a1f6b1d84ad2
Joshua Ohana <joshua.ohana@gmail.com>
2017-07-05 15:19:00 -0700

    got hours in, added manager, misc cleanup and org

--- src/app/features/time-expenses/time-expenses.component.ts
+++ src/app/features/time-expenses/time-expenses.component.ts
@@ -5,6 +5,7 @@ import { BaseComponent } from '../../shared/components/base.component';
 import { WeekDateRange } from '../../models/Date';
 
 import { TimesheetCardComponent } from './timesheet-card/timesheet-card.component';
+import { Timecard, TimecardSection } from './timesheet-card/timecard.model';
 
 import * as moment from 'moment';
 import * as _ from 'lodash';
@@ -14,8 +15,7 @@ import * as _ from 'lodash';
     styles: [],
     templateUrl: './time-expenses.component.html'
 })
-// export class TimeExpensesComponent extends BaseComponent implements OnInit {
-export class TimeExpensesComponent implements OnInit {
+export class TimeExpensesComponent extends BaseComponent implements OnInit {
 
   private timerecords: Array<any>; // TODO properly type
 
@@ -30,11 +30,11 @@ export class TimeExpensesComponent implements OnInit {
   @ViewChild('timesheets') timesheetsComponent: TimesheetCardComponent;
 
   constructor(protected injector: Injector, private activatedRoute: ActivatedRoute) {
-    // super(injector, [
-    //   { service: 'TimeRecordsService', callback: 'timeRecordsCallback' }
-    // ]);
+    super(injector, [
+      { service: 'TimeRecordsService', callback: 'timeRecordsCallback' }
+    ]);
 
-    // this.loading = true;
+    this.loading = true;
 
     this.groupTimesheetsBy = 'employee';
     this.showFilter = 'all';
@@ -46,37 +46,32 @@ export class TimeExpensesComponent implements OnInit {
     this.activatedRoute.params.subscribe(params => {
       this.view = params['view'];
     });
-    // this.buildTimesheets();
   }
 
   timeRecordsCallback(response) {
-    // this.loading = false;
-    // this.timerecords = response.Value;
-    // this.buildTimesheets();
+    this.loading = false;
+    this.timerecords = response.Value;
+    this.buildTimesheets();
   }
 
   groupTimesheets(grouping: string) {
     this.groupTimesheetsBy = grouping;
-    // grouping done by timesheet-card component
     this.buildTimesheets();
   }
 
   dateChanged(e) {
     this.dateRange = e;
-    // TODO do date range filter on timerecords
-    // if (this.loading) return;
     this.buildTimesheets();
   }
 
   filterResults(filter: string) {
     if (filter) this.showFilter = filter;
-    // TODO do mine vs all filter on timerecords
     this.buildTimesheets();
   }
 
   buildTimesheets() {
-    this.timesheetsComponent.buildTimesheets(fakeTimeRecords, this.dateRange, this.groupTimesheetsBy);
-    // this.timesheetsComponent.buildTimesheets(this.timerecords, this.dateRange, this.groupTimesheetsBy);
+  if (this.loading) return;
+    this.timesheetsComponent.buildTimesheets(this.timerecords, this.dateRange, this.groupTimesheetsBy, this.showFilter);
   }
 
   newTimesheet() {}
@@ -88,158 +83,3 @@ export class TimeExpensesComponent implements OnInit {
 
   exportTime() {}
 }
-
-
-const fakeTimeRecords = [
-  {
-    Employee: {
-      Id: 'employjo',
-      FirstName: 'Joshua',
-      LastName: 'Ohana'
-    },
-    Project: {
-      Id: 'projesub',
-      Name: 'Esub'
-    },
-    CostCode: {
-      Id: 'ccplumbing',
-      Name: 'Plumbing'
-    },
-    SystemPhase: {
-      Id: 'syspha1',
-      Name: 'Primary'
-    },
-    Hours: {
-      Date: '2017-06-29T13:15:42.233Z',
-      DoubleTime: 0,
-      Overtime: 0,
-      RegularTime: 2
-    },
-    TimeRecordStatus: 'Pending',
-    Comments: [{
-      Id: 'commentjo1',
-      Value: 'ima comment',
-      User: {
-        FirstName: 'Joshua',
-        LastName: 'Ohana',
-        ProfilePicUrl: 'https://tinyurl.com/luuvldd'
-      }
-    }]
-  },
-  {
-    Employee: {
-      Id: 'employjo',
-      FirstName: 'Joshua',
-      LastName: 'Ohana'
-    },
-    Project: {
-      Id: 'projesub',
-      Name: 'Esub'
-    },
-    CostCode: {
-      Id: 'ccelectrical',
-      Name: 'Electrical'
-    },
-    SystemPhase: {
-      Id: 'syspha1',
-      Name: 'Primary'
-    },
-    Hours: {
-      Date: '2017-06-29T13:15:42.233Z',
-      DoubleTime: 0,
-      Overtime: 0,
-      RegularTime: 6
-    },
-    TimeRecordStatus: 'Rejected',
-    Comments: [{
-      Id: 'commentjo1',
-      Value: 'ima comment',
-      User: {
-        FirstName: 'Joshua',
-        LastName: 'Ohana',
-        ProfilePicUrl: 'https://tinyurl.com/luuvldd'
-      }
-    }]
-  },
-  {
-    Employee: {
-      Id: 'employjo',
-      FirstName: 'Joshua',
-      LastName: 'Ohana'
-    },
-    Project: {
-      Id: 'projesub',
-      Name: 'Esub'
-    },
-    CostCode: {
-      Id: 'ccelectrical',
-      Name: 'Electrical'
-    },
-    SystemPhase: {
-      Id: 'syspha1',
-      Name: 'Primary'
-    },
-    Hours: {
-      Date: '2017-06-28T13:15:42.233Z',
-      DoubleTime: 4,
-      Overtime: 4,
-      RegularTime: 0
-    },
-    TimeRecordStatus: 'Pending',
-    Comments: [{
-      Id: 'commentjo3',
-      Value: 'ima comment commentjo3',
-      User: {
-        FirstName: 'Joshua',
-        LastName: 'Ohana',
-        ProfilePicUrl: 'https://tinyurl.com/luuvldd'
-      }
-    }, {
-      Id: 'commentjo5',
-      Value: 'ima comment commentjo5',
-      User: {
-        FirstName: 'Joshua',
-        LastName: 'Ohana',
-        ProfilePicUrl: 'https://tinyurl.com/luuvldd'
-      }
-    }]
-  },
-  {
-    Employee: {
-      Id: 'employat',
-      FirstName: 'Alex',
-      LastName: 'Takabayashi'
-    },
-    Project: {
-      Id: 'projesub',
-      Name: 'Esub'
-    },
-    CostCode: {
-      Id: 'ccelectrical',
-      Name: 'Electrical'
-    },
-    SystemPhase: {
-      Id: 'syspha1',
-      Name: 'Primary'
-    },
-    Hours: {
-      Date: '2017-06-26T13:15:42.233Z',
-      DoubleTime: 0,
-      Overtime: 0,
-      RegularTime: 8
-    },
-    TimeRecordStatus: 'Approved',
-    Comments: [{
-      Id: 'commentjo1',
-      Value: 'ima comment commentjo4',
-      User: {
-        FirstName: 'Joshua',
-        LastName: 'Ohana',
-        ProfilePicUrl: 'https://tinyurl.com/luuvldd'
-      }
-    }]
-  },
-
-]
-
-// Pending,
--- src/app/features/time-expenses/timesheet-card/timesheet-card.component.ts
+++ src/app/features/time-expenses/timesheet-card/timesheet-card.component.ts
@@ -1,20 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Injector } from '@angular/core';
+import { MdDialog } from '@angular/material';
 
 import { WeekDateRange, WeekDateRangeDetails } from '../../../models/Date';
 import { Timecard, TimecardSection } from './timecard.model';
 
+import { BaseComponent } from '../../../shared/components/base.component';
 import { CommentsComponent } from '../../../shared/components/comments.component';
 
-import { MdDialog } from '@angular/material';
+import { TimesheetCardManager } from './timesheet-card.manager';
 
 import * as moment from 'moment';
 import * as _ from 'lodash';
 
 @Component({
     selector: 'esub-timesheet-card',
-    templateUrl: './timesheet-card.component.html'
+    templateUrl: './timesheet-card.component.html',
+    providers: [ TimesheetCardManager ]
 })
-export class TimesheetCardComponent {
+export class TimesheetCardComponent extends BaseComponent {
   @Input() loading: boolean;
 
   public timecards: Array<Timecard>;    // the built cards, what the template will display
@@ -23,24 +26,38 @@ export class TimesheetCardComponent {
   public entityLookupTable: Array<any>;   // local lookup table for entities (Employee, Project) built during timecard buildup
   public moment = moment;
 
-  constructor(public dialog: MdDialog) {
-    this.timecards = [new Timecard()];    // so we have one card to display loader
+  private userId: string;
+
+  constructor(protected injector: Injector, public dialog: MdDialog, public timesheetCardManager: TimesheetCardManager) {
+    super(injector, [
+      { service: 'UserService', callback: 'userServiceCallback'}
+    ]);
+
+    this.timecards = [];
   }
 
   // build timesheets from timerecords, timerecords should already be pre-filtered for date & user
     // valid groupBy is 'employee' or 'project'
-  public buildTimesheets(timerecords: Array<any>, dateRange: WeekDateRange, groupTimesheetsBy: string) {
+  public buildTimesheets(timerecords: Array<any>, dateRange: WeekDateRange, groupTimesheetsBy: string, showFilter: string) {
+    if (!timerecords || timerecords.length === 0) return;
 
     this.entityLookupTable = [];
 
     // build date labels
-    this.dateRange = this.buildWeekDateRangeDetails(dateRange);
+    this.dateRange = this.timesheetCardManager.buildWeekDateRangeDetails(dateRange);
 
     // filter results by date
     timerecords = _.filter(timerecords, timerecord => {
       return dateRange.startDate.isSameOrBefore(timerecord.Hours.Date) &&
         dateRange.endDate.isSameOrAfter(timerecord.Hours.Date);
     });
+
+    // filter results to active user only
+    if (showFilter === 'mine') {
+      timerecords = _.filter(timerecords, timerecord => {
+        return timerecord.Employee.Id === this.userId;
+      });
+    }
 
     // build dictionary based on grouping
     const groupedTimerecords = _.groupBy(timerecords, timerecord => {
@@ -69,107 +86,7 @@ export class TimesheetCardComponent {
     }
 
     // insert Hour objects at each nesting level and organize
-    this.timecards = this.buildTimecard(cards);
-    console.log('THIS.TIMECARDS', this.timecards);
-  }
-
-  // attaches an hours @ day object (plus other supplemantary info inside that), attached to each group, at each grouping level
-  buildTimecard(cards: Array<any>): Array<any> {
-
-    cards.forEach(card => {
-      // each card
-
-      for (const sectionKey in card.sections) {
-        if (card.sections.hasOwnProperty(sectionKey)) {
-          // each section of each card (anti-group)
-          const section = card.sections[sectionKey];
-
-
-          for (const systemPhaseKey in section) {
-            if (section.hasOwnProperty(systemPhaseKey)) {
-              // each systemPhase in each section
-              const systemPhase = section[systemPhaseKey];
-
-              for (const costCodeKey in systemPhase) {
-                if (systemPhase.hasOwnProperty(costCodeKey)) {
-                  // each costCode array in each systemPhase
-                  const costCodes = systemPhase[costCodeKey];
-                  // each costCode entry here is an array of objects with an hours param (for hours worked on a specific day)
-                  // each costCode entry here should eqate to one 'section' within the UI
-                  // let's compile this codeCode array into a single obj with all the info we need
-                  //     grouping, systemPhase, codeCode, list of each day in week with associated day's comments
-
-                  const finalCostCode = {
-                    grouping: costCodes[0].grouping,
-                    systemPhase: costCodes[0].systemPhase,
-                    costCode: costCodes[0].costCode,
-                    days: {}
-                  }
-
-                  // build empty days object with hours
-                  this.dateRange.forEach(date => {
-                    const dateKey = date.date.format('MM-DD-YYYY');
-                    finalCostCode.days[dateKey] = {
-                      hours: 0,   // start at 0 and sum up all hours
-                      selected: false,
-                      comments: [],
-                      statusRejected: false,
-                      statusPending: false
-                    };
-                  });
-
-                  // sum up hours for use in finalCostCode.days[dateKey].hours
-                  costCodes.forEach(costCode => {
-                    const dateKey = moment(costCode.hours.Date).format('MM-DD-YYYY');
-                    finalCostCode.days[dateKey].hours += costCode.hours.DoubleTime + costCode.hours.Overtime + costCode.hours.RegularTime;
-
-                    // keep track of total status for entire day
-                    switch (costCode.status) {
-                      case 'Rejected':
-                        finalCostCode.days[dateKey].statusRejected = true;
-                        break;
-                      case 'Pending':
-                        finalCostCode.days[dateKey].statusPending = true;
-                        break;
-                      case 'Approved':
-                        break;
-                    }
-
-                    // append comments
-                    finalCostCode.days[dateKey].comments = _.concat(finalCostCode.days[dateKey].comments, costCode.comments);
-                  });
-
-                  //  build up approval status
-                  for (const dateKey in finalCostCode.days) {
-                    if (finalCostCode.days.hasOwnProperty(dateKey)) {
-                      const day = finalCostCode.days[dateKey];
-                      let status: string;
-
-                      if (day.statusRejected) {
-                        status = 'Rejected';
-                      } else if (day.statusPending) {
-                        status = 'Pending'
-                      } else {
-                        status = 'Approved'
-                      }
-
-                      day.status = status;
-                      delete day.statusRejected;
-                      delete day.statusPending;
-                    }
-                  }
-
-                  // replace original obj
-                  systemPhase[costCodeKey] = finalCostCode;
-                }
-              }
-            }
-          }
-        }
-      }
-    });
-
-    return cards;
+    this.timecards = this.timesheetCardManager.buildTimecard(cards, this.dateRange);
   }
 
   // creates sections (within a project or employee)
@@ -183,8 +100,6 @@ export class TimesheetCardComponent {
       this.saveEntity(timerecord, 'Project');
       this.saveEntity(timerecord, 'SystemPhase');
       this.saveEntity(timerecord, 'CostCode');
-
-      // console.log('TIMERECORD.TIMERECORDSTATUS', timerecord.TimeRecordStatus)
 
       sections.push({
         grouping: groupTimesheetsBy === 'employee' ? this.getEntityName(timerecord.Project.Id) : this.getEntityName(timerecord.Employee.Id),
@@ -216,26 +131,6 @@ export class TimesheetCardComponent {
     }
 
     return groupedSections;
-  }
-
-  // builds labels to display date range
-  buildWeekDateRangeDetails(dateRange: WeekDateRange) {
-
-    const weekDateRangeDetails = new Array<WeekDateRangeDetails>();
-    const day = dateRange.startDate.clone();
-
-    while (day.isSameOrBefore(dateRange.endDate, 'days')) {
-
-      weekDateRangeDetails.push({
-        dayString: day.format('dd'),
-        dateString: day.format('MMM. Do'),
-        date: day.clone()
-      });
-
-      day.add(1, 'days');
-    }
-
-    return weekDateRangeDetails;
   }
 
   // saves guid based entity to local lookup table for easy reference
@@ -291,5 +186,9 @@ export class TimesheetCardComponent {
     commentsDialogRef.afterClosed().subscribe(result => {
       // modal closed
     });
+  }
+
+  userServiceCallback(user) {
+    this.userId = user.Id;
   }
 }
