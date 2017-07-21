d3ea52cc068726112a783c205a26188e93ab247d
Alex T <alext@esub.com>
2017-07-21 12:47:24 -0700

    [SHK-694] Updated SignalR implementation. Needs discussion

--- src/app/home/trackpoint/trackpoint.component.ts
+++ src/app/home/trackpoint/trackpoint.component.ts
@@ -29,27 +29,10 @@ export class TrackpointNavigationComponent implements OnInit {
     hubConnection;
     hubProxy;
 
-  constructor(private route: ActivatedRoute, @Inject(SignalrWindow) private window: SignalrWindow) {
-
-      const connection = this.window.$.hubConnection();
-      connection.url = 'http://test.develop.shasta.esubonline.com/signalr';
-      this.hubProxy = connection.createHubProxy('testhub');
-
-      this.registerOnServerEvents();
-
-     connection.start({ withCredentials: false, transport: 'serverSentEvents'})
-          .done(function (data) {
-              console.log('connected', data.transport.name);
-
-          })
-          .fail(function (a) {
-              console.log('not connected'+ a);
-          });
-
-
-  }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+
     this.route.params
       .subscribe(params => {
         const trackPointId = params['id'];
@@ -61,12 +44,45 @@ export class TrackpointNavigationComponent implements OnInit {
         });
       });
 
+      setTimeout(() => {
+
+          // this.hubConnection = $['connection'];
+          // this.hubConnection.hub.url = 'http://test.develop.shasta.esubonline.com/signalr';
+          // this.hubConnection.hub.logging = true;
+          //
+          // this.hubConnection.hub.start({ withCredentials: false, transport: 'serverSentEvents'})
+          //     .done((response) => {
+          //         console.log('connected', response.transport.name);
+          //         this.hubProxy = this.hubConnection.testHub;
+          //         console.log(this.hubProxy.client);
+          //         this.hubProxy.client.send = function (message) {
+          //             console.log('connected', message);
+          //         };
+          //     })
+          //     .fail((error) => {
+          //         console.log('not connected' + error);
+          //     });
+
+           const connection = $['hubConnection']();
+           connection.url = 'http://test.develop.shasta.esubonline.com/signalr';
+           this.hubProxy = connection.createHubProxy('testhub');
+
+           this.registerOnServerEvents();
+
+          connection.start({ withCredentials: false, transport: 'serverSentEvents'})
+               .done(function (data) {
+                   console.log('connected', data.transport.name);
+
+               })
+               .fail(function (a) {
+                   console.log('not connected'+ a);
+               });
+      });
   }
 
     private registerOnServerEvents(): void {
-        this.hubProxy.on('send', (data) => {
-            console.log('received in SignalRService: ' + JSON.stringify(data));
-
+        this.hubProxy.on('send', (name, message) => {
+            console.log(name, message);
         });
     }
 }
