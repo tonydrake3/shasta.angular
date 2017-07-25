27f282d4e7e4476f8ca13eb8985d8f5414b913b8
Alex T <alext@esub.com>
2017-07-25 12:14:00 -0700

    [SHK-694] Added popover global click handling directive

--- src/app/home/shared/directives/click-close.directive.ts
+++ src/app/home/shared/directives/click-close.directive.ts
@@ -9,21 +9,27 @@ export class ClickCloseDirective {
     @HostListener('document:click', ['$event']) onClick(event) {
 
         if (this._elementRef.nativeElement.contains(event.target)) {
-            // console.log('ClickCloseDirective inside', event, this._elementRef);
+
+            // Placeholder for cases where you want to handle click on event target
         } else {
-            console.log('ClickCloseDirective outside', event, this._elementRef, event.target.contains('notificationButton'));
-            // this.closePopover();
+
+            // Case to handle clicking outside of target element.
+            this.closePopover(event);
         }
     }
 
-    constructor(private _elementRef: ElementRef, private _popoverService: PopoverService) {
+    constructor(private _elementRef: ElementRef, private _popoverService: PopoverService) {}
 
-        console.log('ElementRef', this._elementRef);
-    }
 
-    closePopover () {
+    /******************************************************************************************************************
+     * Private Methods
+     ******************************************************************************************************************/
 
-        if (this._elementRef.nativeElement.className.indexOf('popover') > -1) {
+    private closePopover (event) {
+
+        // Special check to see if notifications button was event target.
+        // Check is necessary to prevent closing the notifications pop-up
+        if (event.target.offsetParent.id !== 'notifications' && event.target.id !== 'notifications') {
 
             this._popoverService.closePopover();
         }
