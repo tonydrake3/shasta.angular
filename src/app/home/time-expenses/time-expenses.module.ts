7100321e4a952dfcb27646ed7c31094e38fc00b3
Guillermo Alvarez <guillermoa@esub.com>
2017-10-16 16:28:39 -0700

    a bit more cleanup after merging

--- src/app/home/time-expenses/time-expenses.module.ts
+++ src/app/home/time-expenses/time-expenses.module.ts
@@ -29,7 +29,6 @@ import { EnterTimePreloadManager } from './enter-time/enter-time-preload.manager
 import { EnterTimeBatchService } from './enter-time/enter-time-batch.service';
 import { EnterTimeFilterService } from './enter-time/enter-time-filter.service';
 import { EnterTimeTransformService } from './enter-time/enter-time-transform.service';
-import {EnterTimeGridBuilderService} from './enter-time/enter-time-grid-builder.service';
 import {EnterTimeNoteDialogComponent} from './enter-time/enter-time-note-dialog/enter-time-note-dialog.component';
 import {EntityDisplayFormatterService} from '../shared/services/entity-display-formatter.service';
 
@@ -66,7 +65,6 @@ import {EntityDisplayFormatterService} from '../shared/services/entity-display-f
         TimeRecordsService,
         EnterTimeBatchService,
         EnterTimeFilterService,
-        EnterTimeGridBuilderService,
         EnterTimeManager,
         EnterTimeStatusService,
         EnterTimePreloadManager,
