37b678e83bc77df6934278b37351bc377b1a0b20
Bahar <bahar@Bahars-MacBook-Pro.local>
2017-10-24 08:58:04 -0700

    fixed the routing problem in settings

--- src/app/home/time-expenses/time-expenses-routing.module.ts
+++ src/app/home/time-expenses/time-expenses-routing.module.ts
@@ -1,15 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
-
 import { TimeExpensesComponent } from './time-expenses.component';
-import { EnterTimeComponent } from './enter-time/enter-time.component';
-
 import {TimeSettingsComponent} from './settings/settings.component';
+import { EnterTimeComponent } from './enter-time/enter-time.component';
 import { CanDeactivateGuard } from '../shared/services/guards/deactivate-guard.service';
 export const TimeExpensesRoutes: Routes = [
   { path: '', redirectTo: '/time/timesheets', pathMatch: 'full' },
   { path: 'enter', component: EnterTimeComponent, canDeactivate: [CanDeactivateGuard] },
-  { path: ':view', component: TimeExpensesComponent },
-  { path: 'settings', component: TimeSettingsComponent }
+  { path: 'settings', component: TimeSettingsComponent },
+  { path: ':view', component: TimeExpensesComponent }
 ];
 
 export const TimeExpensesRoutingModule = RouterModule.forChild(TimeExpensesRoutes);
